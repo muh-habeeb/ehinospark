334935b8235cd44a1159b20c2b1bd09a0dd169b5
muh-habeeb <muhammadhabeebrahmam@gmail.com>
2025-10-15 02:06:34 +0530

    new:vercel analytics[speedInsights] added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,6 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children}
+        <SpeedInsights />
+
         <Toaster />
       </body>
     </html>
